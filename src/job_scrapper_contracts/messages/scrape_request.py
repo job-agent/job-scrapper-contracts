af64866c15e26f073e35b29519b8cbbec7e032b0
EvilMangos <jchamets@gmail.com>
2025-11-25 16:53:25 -0500

    ID-18: add existing_urls parameter to ScrapeJobsFilter

--- src/job_scrapper_contracts/messages/scrape_request.py
+++ src/job_scrapper_contracts/messages/scrape_request.py
@@ -9,11 +9,11 @@ class ScrapeJobsRequest(TypedDict, total=False):
     """Request message for scraping jobs.
 
     Attributes:
-        filter: Filtering options for the request
+        filters: Filtering options for the request
         timeout: Request timeout in seconds
         batch_size: Optional batch size for streaming callbacks
     """
 
-    filter: ScrapeJobsFilter
+    filters: ScrapeJobsFilter
     timeout: int
     batch_size: int
